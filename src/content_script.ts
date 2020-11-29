f47356ed6d0704d489daa9e38c9bf4802628fc87
miayang0513 <a16168972@gmail.com>
2020-11-29 22:32:23 +0800

    feat: make element draggable

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,26 +1,58 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   const { action } = msg
-  if (action) {
-    if (action === 'changeTitle') {
-      const title = document.querySelector('span.houseInfoTitle')
-      if (title) title.innerHTML = 'Testing'
-      sendResponse('changeTitle')
-      console.log('injected')
-    } else if (action === 'insertPanel') {
-      const bodyElement = document.querySelector('body')
-      const mainElement = document.createElement('div')
-      mainElement.setAttribute('id', 'zotsu-app')
-      const x = window.innerWidth - 140
-      const y = window.innerHeight - 160
-      mainElement.style.transform = `translate(${x}px, ${y}px)`
 
-      mainElement.innerHTML = `
+  if (!action) {
+    return
+  }
+
+  if (action === 'changeTitle') {
+    const title = document.querySelector('span.houseInfoTitle')
+    if (title) title.innerHTML = 'Testing'
+    sendResponse('changeTitle')
+  } else if (action === 'insertPanel') {
+    const bodyElement = document.querySelector('body')
+    const mainElement = document.createElement('div')
+    mainElement.setAttribute('id', 'zotsu-app')
+    const left = window.innerWidth - 140
+    const top = window.innerHeight - 160
+    mainElement.style.left = left + 'px'
+    mainElement.style.top = top + 'px'
+
+    mainElement.innerHTML = `
         <div class="app-container">
           <div class="app-icon"></div>
-          <div class="app-panel"></div>
         </div>
       `
-      bodyElement?.prepend(mainElement)
-    }
+    makeDraggable(mainElement)
+    bodyElement?.prepend(mainElement)
+    sendResponse('insertPanel')
   }
 })
+
+function makeDraggable(element: HTMLElement): void {
+  const dragPosition = {
+    currentX: 0,
+    currentY: 0,
+    initialX: 0,
+    initialY: 0,
+    xOffset: 0,
+    yOffset: 0
+  }
+  element.addEventListener('mousedown', (event) => {
+    dragPosition.initialX = event.clientX - dragPosition.xOffset
+    dragPosition.initialY = event.clientY - dragPosition.yOffset
+
+    document.onmousemove = (event) => {
+      event.preventDefault()
+      dragPosition.currentX = event.clientX - dragPosition.initialX
+      dragPosition.currentY = event.clientY - dragPosition.initialY
+      dragPosition.xOffset = dragPosition.currentX
+      dragPosition.yOffset = dragPosition.currentY
+      element.style.transform = `translate(${dragPosition.currentX}px, ${dragPosition.currentY}px)`
+    }
+    document.onmouseup = () => {
+      document.onmouseup = null
+      document.onmousemove = null
+    }
+  })
+}
