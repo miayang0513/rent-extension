fac77c626fccba052ebb8f6b4e177a4fe965fddb
miayang0513 <a16168972@gmail.com>
2020-12-02 00:30:07 +0800

    feat: insert panel of median and overall rating

--- src/content_script.ts
+++ src/content_script.ts
@@ -29,7 +29,45 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     const elementAppIcon = document.createElement('div')
     elementAppIcon.setAttribute('class', 'app-icon')
 
-    elementContainer.prepend(elementAppIcon)
+    const elementAppPanel = document.createElement('div')
+    elementAppPanel.setAttribute('class', 'app-panel')
+
+    const overallRating = [
+      {
+        name: '環境噪音',
+        rating: 4.0
+      },
+      {
+        name: '交通便利',
+        rating: 2.8
+      },
+      {
+        name: '房屋狀況',
+        rating: 0.9
+      },
+      {
+        name: '房東評價',
+        rating: 5.0
+      }
+    ]
+    const elementSheet = generateOverallRatingSheet(overallRating)
+    const elementMedian = generateMedianPanel(99999)
+
+    elementAppPanel.addEventListener('click', (e) => {
+      e.stopPropagation()
+      if (elementAppPanel.children[0].classList.contains('panel--expand')) {
+        elementAppPanel.children[0].classList.remove('panel--expand')
+        elementSheet.style.display = 'none'
+      } else {
+        elementAppPanel.children[0].classList.add('panel--expand')
+        elementSheet.style.display = 'block'
+      }
+    })
+
+    elementAppPanel.append(elementMedian)
+    elementAppPanel.append(elementSheet)
+    elementContainer.append(elementAppIcon)
+    elementContainer.append(elementAppPanel)
     document.querySelector('body')?.prepend(elementContainer)
     sendResponse('insertPanel')
   }
@@ -75,4 +113,57 @@ function makeDraggable(element: HTMLElement, clickHandler?: { (): void }): void 
       element.addEventListener('click', clickHandler)
     }
   })
+}
+
+function generateMedianPanel(median: number): HTMLElement {
+  const element = document.createElement('div')
+  element.setAttribute('class', 'panel')
+  element.innerHTML = `
+    <div class="panel__upper">
+      <span>區域中位數</span>
+      <i></i>
+    </div>
+    <div class="panel__lower">
+      <em>${median}</em>
+      <span>元/月</span>
+    </div>
+  `
+  return element
+}
+
+function generateOverallRatingSheet(overallRating: Array<{ name: string; rating: number }>): HTMLElement {
+  const elementSheet = document.createElement('div')
+  elementSheet.style.display = 'none'
+  elementSheet.setAttribute('class', 'sheet')
+
+  let htmlContent = ''
+  overallRating.forEach((item) => {
+    htmlContent += `
+      <div class="sheet__item">
+        <div>
+          <span>${item.name}</span>
+          <em>${item.rating}</em>
+        </div>
+    `
+    htmlContent += `
+        <div class="sheet__item__rating">
+    `
+    for (let i = 0; i < Math.floor(item.rating); i++) {
+      htmlContent += `
+          <i class="checked"></i>
+      `
+    }
+    for (let i = 0; i < 5 - Math.floor(item.rating); i++) {
+      htmlContent += `
+          <i></i>
+      `
+    }
+    htmlContent += `
+        </div>
+      </div>
+    `
+  })
+  elementSheet.innerHTML = htmlContent
+
+  return elementSheet
 }
